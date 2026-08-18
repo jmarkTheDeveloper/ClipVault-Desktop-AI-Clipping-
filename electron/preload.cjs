9fc8facd6a42e7f609f42b88d4cb241c150a29b0
jmarkTheDeveloper <jaemarkalmeria08@gmail.com>
2026-08-18 18:18:56 +0800

    Initial commit: ClipVault Desktop AI Clipping Software with Folder Management & Multi-Clip Studio

---
+++ electron/preload.cjs
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// We expose a secure API to the window object for React to use
+contextBridge.exposeInMainWorld('electronAPI', {
+  getYoutubeInfo: (url) => ipcRenderer.invoke('get-youtube-info', url),
+  showOpenDialog: (options) => ipcRenderer.invoke('show-open-dialog', options),
+  selectDirectory: (defaultPath) => ipcRenderer.invoke('select-directory', defaultPath),
+  openPath: (folderPath) => ipcRenderer.invoke('open-path', folderPath),
+  showItemInFolder: (filePath) => ipcRenderer.invoke('show-item-in-folder', filePath),
+});
